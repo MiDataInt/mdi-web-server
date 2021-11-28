276d12b9adb2164a70831432548d54ef91c7a9b4
Tom Wilson <wilsonte@umich.edu>
2021-11-28 00:12:52 +0000

    enable app-server debug watch

--- mdi-apps-server/install-mdi.sh
+++ mdi-apps-server/install-mdi.sh
@@ -3,14 +3,14 @@
 
 # copy in the up-to-date config files
 mkdir -p $MDI_DIR/config
-cp /srv/config/* $MDI_DIR/config
+cp -r /srv/config/* $MDI_DIR/config
+
+# update the installation manager, if available since Docker image was created
+Rscript -e "remotes::install_github('MiDataInt/mdi-manager')"
 
 # use the manager to install the framework repositories and R packages
 Rscript -e "mdi::install(Sys.getenv('MDI_DIR'), confirm = FALSE, addToPATH = TRUE)"
 
 # remove a hacky DT css file with !important that blocks us from controlling css ourselves
 # needed since shinydashboard page functions don't allow us to force our css to load last
-ls $MDI_DIR/library/*/DT/htmlwidgets/lib/datatables/css/jquery.dataTables.extra.css
-
-# # remove dangerous code not applicable to server mode
-# TODO: get this path
+echo "" > $MDI_DIR/library/*/DT/htmlwidgets/lib/datatables/css/jquery.dataTables.extra.css
--- mdi-apps-server/start-app-server.R
+++ mdi-apps-server/start-app-server.R
@@ -8,6 +8,6 @@ mdi::run(
   url = paste0("https://", Sys.getenv('WEB_DOMAIN'), '/'),
   port = 3838, # reverse proxy responds on 443, R responds on 3838 on the docker public-server network
   browser = FALSE,
-  debug = FALSE,
+  debug = as.logical(Sys.getenv('IS_DEBUG')),
   developer = FALSE # !! must never be TRUE on a public server !!
 )
