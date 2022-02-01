06ecb07f37d21f87f29619ecfe543b9e51bc0f11
Thomas E Wilson <wilsonte@umich.edu>
2022-02-01 12:01:07 -0500

    install/image efficiency, remove config yml

--- mdi-apps-server/install-mdi.sh
+++ mdi-apps-server/install-mdi.sh
@@ -5,11 +5,20 @@
 mkdir -p $MDI_DIR/config
 cp -r /srv/config/* $MDI_DIR/config
 
-# update the installation manager, if new since Docker image was created
-Rscript -e "remotes::install_github('MiDataInt/mdi-manager')"
+# set the number of cores for parallel R package compilation
+export N_CPU=`nproc`
 
-# use the manager to install the framework repositories and R packages
+# set environment variable to honor system library in server images
+export MDI_SYSTEM_R_LIBRARY=/usr/local/lib/R/site-library
+
+# update the installation manager, if new since Docker image was created
+Rscript -e "remotes::install_github('MiDataInt/mdi-manager', Ncpus = $N_CPU)"
+
+# use the manager to install the MDI repositories and R packages
 Rscript -e "mdi::install(Sys.getenv('MDI_DIR'), confirm = FALSE, addToPATH = TRUE)"
+
+# remove R package tarballs to control image/container size
+rm -f /tmp/Rtmp*/downloaded_packages/*.tar.gz
 
 # remove a hacky DT css file with !important that blocks us from controlling css ourselves
 # needed since shinydashboard page functions don't allow us to force our css to load last
--- mdi-apps-server/start-app-server.R
+++ mdi-apps-server/start-app-server.R
@@ -1,13 +1,16 @@
+# set environment variable to honor system library in server images
+Sys.setenv(MDI_SYSTEM_R_LIBRARY = "/usr/local/lib/R/site-library")
+
 # launch the web server
 mdi::run(
-  mdiDir = Sys.getenv('MDI_DIR'),
+  mdiDir = Sys.getenv("MDI_DIR"),
   dataDir = NULL,
   hostDir = NULL,
   mode = "server",
   install = TRUE,
-  url = paste0("https://", Sys.getenv('WEB_DOMAIN'), '/'),
+  url = paste0("https://", Sys.getenv("WEB_DOMAIN"), "/"),
   port = 3838, # reverse proxy responds on 443, R responds on 3838 on the docker public-server network # nolint
   browser = FALSE,
-  debug = as.logical(Sys.getenv('IS_DEBUG')),
-  developer = FALSE # !! must never be TRUE on a public server !!
+  debug = as.logical(Sys.getenv("IS_DEBUG")),
+  developer = FALSE # never TRUE on a public server
 )
